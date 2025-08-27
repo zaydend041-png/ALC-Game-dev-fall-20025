e7a6be166b98e1d4a051b661c4e9c246cd12cb68
zaydend041-png <zayden.d041@stu.nebo.edu>
2025-08-27 14:16:13 -0600

    tank game

---
+++ Prototype1 tank game/Assets/Scenes/player controller.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float speed;
+    public float rotspeed;
+    public float hInput;
+    public float vInput;
+    public float jumpforce;
+    public Rigidbody playerRB;
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.up, hInput * rotspeed * Time.deltaTime); //Move the vehicle left and right (side-to-side)
+        transform.Translate(Vector3.forward * vInput * speed * Time.deltaTime);//Moves the vehicle forward and backwards
+    }
+}
