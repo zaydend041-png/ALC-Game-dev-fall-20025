9e07f47be4ad8e648fd0bb5c988ee0484897a006
zaydend041-png <zayden.d041@stu.nebo.edu>
2025-10-14 14:18:27 -0600

    lots of pixels

--- 2d Platformer/Assets/Scripts/PlayerController2D.cs
+++ 2d Platformer/Assets/Scripts/PlayerController2D.cs
@@ -14,55 +14,37 @@ public class PlayerController2D : MonoBehaviour
     public int bottomBound = -4;
     [Header("Score")]
     public int score;
-
-
-
     public Rigidbody2D rig;
     public TextMeshProUGUI scoreText;
-
-
     public void AddScore (int amount)
     {
-
         score += amount;
-
         scoreText.text = "Score: " + score;
     }
-
-
     void FixedUpdate ()
     {
-
         float moveInput = Input.GetAxisRaw("Horizontal");
-
         rig.linearVelocity = new Vector2(moveInput * movespeed, rig.linearVelocity.y);
-
     }
-
     void Update()
     {
-
         if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
         {
             isGrounded = false;
             rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-
         if(transform.position.y < bottomBound)
         {
             GameOver();
         }
     }
-
     void OnCollisionEnter2D (Collision2D collision)
     {
-
         if(collision.GetContact(0).normal == Vector2.up)
         {
             isGrounded = true;
         }
     }
-
     public void GameOver()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
